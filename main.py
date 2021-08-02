76177752098ab7a3ac62de670c0fc3a706763416
0ryX <Hak_0ryX@protonmail.com>
2021-08-01 22:24:14 -0400

    Sunday

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 
 import mlbgame
 import random
-from datetime import date
+from datetime import date, timedelta
+import requests
+
+url = 'https://mlb.jcompsolu.com/api/game'
+
 IN_PROGRESS = 'IN_PROGRESS'
 
-todays_date = date.today()
+todays_date = date.today() +  timedelta(days=1)
 games = mlbgame.day(todays_date.year, todays_date.month, todays_date.day)
 for game in games:
     if game.game_status != IN_PROGRESS:
@@ -18,6 +22,8 @@ for game in games:
         prediction['home_team'] = game.home_team
         prediction['away_team'] = game.away_team
         prediction['game_id'] = game.game_id
-        prediction['win_percentage'] = random.uniform(40.0, 75.0)
+        prediction['win_percentage'] = random.uniform(51.0, 81.0)
         prediction['winning_predicted_team'] = win_team
-        print(prediction)
+        prediction['game_date'] = todays_date
+        send = requests.post(url, prediction)
+        print(send)
